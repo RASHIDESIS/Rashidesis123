f49e74a43897b06532a0715afb827aa54b187c60
RASHIDESIS <mushhfiq04@gmail.com>
2023-11-28 20:08:16 +0600

    function added

--- Basic/function.py
+++ Basic/function.py
@@ -1,6 +1,18 @@
 str1 = "mushfiq"
+str2 = "rashid"
+# count = 0
+# for i in str1:
+#     count = count +1
+# print(count)
 
-count = 0
-for i in str1:
-    count = count +1
-print(count)
+def custom_len(x,y):
+    count1 = 0
+    count2 = 0
+    for i in x:
+         count1 = count1 +1
+    for j in y:
+         count2 = count2 +1
+    return(count1,count2)
+
+a,b = custom_len(str1,str2)
+print(a,b)
